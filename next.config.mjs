8dfe3e9267001124be55ec2bf4ce6d5ab3114cdf
pongwisanu <pongwisanu.hut@nida.ac.th>
2024-07-26 13:23:56 +0700

    Set up next.js

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/pongwisanu.github.io",
+    assetPrefix: "/pongwisanu.github.io/",
+};
+
+export default nextConfig;
